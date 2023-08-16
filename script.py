75d7b65606bae4285853bdff3308c4baf2f81a44
Sabbir Hossain Ujjal <67465166+VirusProton@users.noreply.github.com>
2023-08-17 03:19:49 +0600

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 
 import os
 import re
+import shutil
 import numpy as np
 import pandas as pd
 import argparse
@@ -25,7 +26,7 @@ parser.add_argument("--resume_dir", type= str, default= CONFIG.test_data_path, h
 args = parser.parse_args()
 
 data_root_path= args.resume_dir
-
+print("Loading Data")
 test_df= load_file(data_root_path)
 test_df['Resume_str']= test_df['Resume_str'].apply(remove_unwanted_chars)
 
@@ -34,13 +35,26 @@ model= ResumeClassifier(CONFIG.model_name_or_path)
 model.load_state_dict(torch.load(CONFIG.model_checkpoint, map_location=  device))
 model.to(device)
 
+curr_dir= os.getcwd()
+for folder in CONFIG.categories:
+    path= os.path.join(curr_dir, folder)
+    if os.path.exists(path) and os.path.isdir(path):
+        pass
+    else:
+        os.makedirs(path)
+
+filenames= test_df.ID.values
 resumes= test_df.Resume_str.values
 predictions= []
 
-for resume in tqdm(resumes):
+print("Running Predictions")
+for filename, resume in zip(filenames, resumes):
     inp= tokenizer(resume, return_token_type_ids= False, truncation= True, max_length= CONFIG.token_max_length, return_tensors= "pt")
     pred= model(**inp).argmax(axis=1).to('cpu').numpy()[0]
-    predictions.append(str(CONFIG.id2label[str(pred)]))
+    pred_cat= str(CONFIG.id2label[str(pred)])
+    predictions.append(pred_cat)
+    ### copying the file to category folder
+    shutil.copy(os.path.join(data_root_path, filename), os.path.join(curr_dir, pred_cat, filename))    
 
 test_df["Predictions"]= predictions
 
