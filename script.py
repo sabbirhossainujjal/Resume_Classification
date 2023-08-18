c2b192ee16fdf32a108118f600630924a74af951
Sabbir Hossain Ujjal <67465166+VirusProton@users.noreply.github.com>
2023-08-18 22:30:36 +0600

    fixed some bugs

--- script.py
+++ script.py
@@ -18,18 +18,23 @@ from utils.preprocessing import *
 ###################################
 set_seed(seed= 42)
 
-device= torch.device("cuda" if torch.cuda.is_available( else 'cpu')
+device= torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 CONFIG.device= device
 
 parser= argparse.ArgumentParser(description= "Resume Classification")
-parser.add_argument("--resume_dir", type= str, default= CONFIG.test_data_path, help= "Path of the test csv")
+parser.add_argument("--resume_dir", type= str, default= CONFIG.resume_path, help= "Path of the test csv")
+parser.add_argument("--output_dir", type= str, default= "results", help= "Path of the output files")
 args = parser.parse_args()
 
 data_root_path= args.resume_dir
+output_path= args.output_dir
+
+## Loding data and apply some preprocessin
 print("Loading Data")
 test_df= load_file(data_root_path)
 test_df['Resume_str']= test_df['Resume_str'].apply(remove_unwanted_chars)
 
+# Loading models and tokenizers
 tokenizer= get_tokenizer(CONFIG.model_name_or_path)
 model= ResumeClassifier(CONFIG.model_name_or_path)
 model.load_state_dict(torch.load(CONFIG.model_checkpoint, map_location=  device))
@@ -37,7 +42,7 @@ model.to(device)
 
 curr_dir= os.getcwd()
 for folder in CONFIG.categories:
-    path= os.path.join(curr_dir, folder)
+    path= os.path.join(curr_dir, output_path ,folder)
     if os.path.exists(path) and os.path.isdir(path):
         pass
     else:
@@ -54,7 +59,7 @@ for filename, resume in zip(filenames, resumes):
     pred_cat= str(CONFIG.id2label[str(pred)])
     predictions.append(pred_cat)
     ### copying the file to category folder
-    shutil.copy(os.path.join(data_root_path, filename), os.path.join(curr_dir, pred_cat, filename))    
+    shutil.copy2(os.path.join(data_root_path, filename), os.path.join(curr_dir, output_path, pred_cat, filename))    
 
 test_df["Predictions"]= predictions
 
